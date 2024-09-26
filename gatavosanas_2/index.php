dab3cf3a3f855972081e6b062204a3b8eca392c8
unknown <test@email.com>
2024-09-26 08:49:02 +0300

    something

---
+++ gatavosanas_2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="aaa.css">
+</head>
+<body>
+    <h1 id="main-heading"> Lorem, ipsum dolor.</h1>
+
+    <p class="some-p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, veniam.</p>
+
+    <div id="form_container">
+        <form action="another_php_file.php" method="post">
+            <label for="first_name">Vards</label>
+            <input type="text" name="first_name" id="first_name">
+            <select name="" id=""></select>
+            <input type="radio" name="" id="">
+            <input type="button" value="submit">
+        </form>
+    </div>
+
+    <script src="aa.js"></script>
+</body>
+</html>
